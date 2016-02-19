3e33b4c64a613ef31e6a9343bce6e63b534a7326
Jacob <Jacob@172.24.50.216>
2016-02-19 09:25:34 -0500

    Finished part 9) edited PageEditController.java

--- WebPageMakerApp/src/wpm/gui/Workspace.java
+++ WebPageMakerApp/src/wpm/gui/Workspace.java
@@ -23,16 +23,13 @@ import javafx.scene.control.TreeItem;
 import javafx.scene.control.TreeView;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.Border;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
-import javafx.scene.paint.Color;
 import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebView;
-import javax.swing.BorderFactory;
 import properties_manager.PropertiesManager;
 import saf.ui.AppGUI;
 import wpm.data.HTMLTagPrototype;
@@ -40,7 +37,6 @@ import saf.AppTemplate;
 import saf.components.AppWorkspaceComponent;
 import static saf.settings.AppStartupConstants.FILE_PROTOCOL;
 import static saf.settings.AppStartupConstants.PATH_IMAGES;
-import wpm.PropertyType;
 import static wpm.PropertyType.TEMP_PAGE_LOAD_ERROR_MESSAGE;
 import static wpm.PropertyType.TEMP_PAGE_LOAD_ERROR_TITLE;
 import static wpm.PropertyType.UPDATE_ERROR_MESSAGE;
@@ -123,142 +119,141 @@ public class Workspace extends AppWorkspaceComponent {
      * data for setting up the user interface.
      */
     public Workspace(AppTemplate initApp) throws IOException {
-	// KEEP THIS FOR LATER
-	app = initApp;
+        // KEEP THIS FOR LATER
+        app = initApp;
 
-	// KEEP THE GUI FOR LATER
-	gui = app.getGUI();
+        // KEEP THE GUI FOR LATER
+        gui = app.getGUI();
 
-	// THIS WILL PROVIDE US WITH OUR CUSTOM UI SETTINGS AND TEXT
-	PropertiesManager propsSingleton = PropertiesManager.getPropertiesManager();
+        // THIS WILL PROVIDE US WITH OUR CUSTOM UI SETTINGS AND TEXT
+        PropertiesManager propsSingleton = PropertiesManager.getPropertiesManager();
 
-	// WE'LL ORGANIZE OUR WORKSPACE COMPONENTS USING A BORDER PANE
-	workspace = new BorderPane();
+        // WE'LL ORGANIZE OUR WORKSPACE COMPONENTS USING A BORDER PANE
+        workspace = new BorderPane();
 
-	// FIRST THE LEFT HALF OF THE SPLIT PANE
-	leftPane = new BorderPane();
+        // FIRST THE LEFT HALF OF THE SPLIT PANE
+        leftPane = new BorderPane();
 
-	// THIS WILL MANAGE ALL EDITING EVENTS
-	pageEditController = new PageEditController((WebPageMaker) app);
+        // THIS WILL MANAGE ALL EDITING EVENTS
+        pageEditController = new PageEditController((WebPageMaker) app);
 
-	// THIS IS THE TOP TOOLBAR
-	tagToolbar = new FlowPane(Orientation.VERTICAL);
-	tagToolbarScrollPane = new ScrollPane(tagToolbar);
-	tagToolbarScrollPane.setFitToHeight(true);
-	tagButtons = new ArrayList();
-	tags = new HashMap();
+        // THIS IS THE TOP TOOLBAR
+        tagToolbar = new FlowPane(Orientation.VERTICAL);
+        tagToolbarScrollPane = new ScrollPane(tagToolbar);
+        tagToolbarScrollPane.setFitToHeight(true);
+        tagButtons = new ArrayList();
+        tags = new HashMap();
 
-	// LOAD ALL THE HTML TAG TYPES
-	FileManager fileManager = (FileManager) app.getFileComponent();
-	DataManager dataManager = (DataManager) app.getDataComponent();
+        // LOAD ALL THE HTML TAG TYPES
+        FileManager fileManager = (FileManager) app.getFileComponent();
+        DataManager dataManager = (DataManager) app.getDataComponent();
 
-	// AND NOW MAKE THE TREE
-	htmlTree = new TreeView();
-	treeScrollPane = new ScrollPane(htmlTree);
-	htmlTree.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
-	htmlTree.getSelectionModel().selectedItemProperty().addListener(e -> {
-	    reloadWorkspace();
-	});
+        // AND NOW MAKE THE TREE
+        htmlTree = new TreeView();
+        treeScrollPane = new ScrollPane(htmlTree);
+        htmlTree.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
+        htmlTree.getSelectionModel().selectedItemProperty().addListener(e -> {
+            reloadWorkspace();
+        });
 
-	// NOW RESET THE TREE
-	HTMLTagPrototype htmlTag = new HTMLTagPrototype(TAG_HTML, true);
-	htmlRoot = new TreeItem(htmlTag);
-	htmlTree.setRoot(htmlRoot);
-	dataManager.setHTMLRoot(htmlRoot);
-	dataManager.reset();
+        // NOW RESET THE TREE
+        HTMLTagPrototype htmlTag = new HTMLTagPrototype(TAG_HTML, true);
+        htmlRoot = new TreeItem(htmlTag);
+        htmlTree.setRoot(htmlRoot);
+        dataManager.setHTMLRoot(htmlRoot);
+        dataManager.reset();
 
         PropertiesManager props = PropertiesManager.getPropertiesManager();
         String imagePath = FILE_PROTOCOL + PATH_IMAGES + "RemoveElement.png";
         Image buttonImage = new Image(imagePath);
-        
+
         Button removeButton = new Button();
         removeButton.setGraphic(new ImageView(buttonImage));
         removeButton.setMaxWidth(BUTTON_TAG_WIDTH);
-	removeButton.setMinWidth(BUTTON_TAG_WIDTH);
-	removeButton.setPrefWidth(BUTTON_TAG_WIDTH);
+        removeButton.setMinWidth(BUTTON_TAG_WIDTH);
+        removeButton.setPrefWidth(BUTTON_TAG_WIDTH);
         tagToolbar.getChildren().add(removeButton);
         removeButton.getStyleClass().add(CLASS_TAG_BUTTON);
-         
-         
-        removeButton.setOnAction(e -> {
-        pageEditController.handleRemoveElementRequest(); 
-        });
-        
-	// AND NOW USE THE LOADED TAG TYPES TO ADD BUTTONS
-	for (HTMLTagPrototype tag : dataManager.getTags()) {
-	    // MAKE THE BUTTON
-            
-	    Button tagButton = new Button(tag.getTagName());
-	    tagButtons.add(tagButton);
-	    tagButton.setMaxWidth(BUTTON_TAG_WIDTH);
-	    tagButton.setMinWidth(BUTTON_TAG_WIDTH);
-	    tagButton.setPrefWidth(BUTTON_TAG_WIDTH);
-            tagToolbar.getChildren().add(tagButton);
-	   
 
-	    // INIT ITS EVENT HANDLER
-	    tagButton.setOnAction(e -> {
-		String tagName = tagButton.getText();
-		HTMLTagPrototype clickedTag = dataManager.getTag(tagName);
-		pageEditController.handleAddElementRequest(clickedTag);
-	    });
+        removeButton.setOnAction(e -> {
+            pageEditController.handleRemoveElementRequest();
+        });
+     
+
+        // AND NOW USE THE LOADED TAG TYPES TO ADD BUTTONS
+        for (HTMLTagPrototype tag : dataManager.getTags()) {
+            // MAKE THE BUTTON
+
+            Button tagButton = new Button(tag.getTagName());
+            tagButtons.add(tagButton);
+            tagButton.setMaxWidth(BUTTON_TAG_WIDTH);
+            tagButton.setMinWidth(BUTTON_TAG_WIDTH);
+            tagButton.setPrefWidth(BUTTON_TAG_WIDTH);
+            tagToolbar.getChildren().add(tagButton);
+            
+            // INIT ITS EVENT HANDLER
+            tagButton.setOnAction(e -> {
+                String tagName = tagButton.getText();
+                HTMLTagPrototype clickedTag = dataManager.getTag(tagName);
+                pageEditController.handleAddElementRequest(clickedTag);
+            });
         }
-        
+
         // AND NOW THE REGION FOR EDITING TAG PROPERTIES
 	tagEditorPane = new GridPane();
-	tagEditorScrollPane = new ScrollPane(tagEditorPane);
-	tagEditorLabel = new Label("Tag Editor");
-	tagPropertyLabels = new ArrayList();
-	tagPropertyTextFields = new ArrayList();
+        tagEditorScrollPane = new ScrollPane(tagEditorPane);
+        tagEditorLabel = new Label("Tag Editor");
+        tagPropertyLabels = new ArrayList();
+        tagPropertyTextFields = new ArrayList();
 
-	// PUT THEM IN THE LEFT
-	leftPane.setLeft(tagToolbarScrollPane);
-	leftPane.setCenter(treeScrollPane);
-	leftPane.setBottom(tagEditorScrollPane);
+        // PUT THEM IN THE LEFT
+        leftPane.setLeft(tagToolbarScrollPane);
+        leftPane.setCenter(treeScrollPane);
+        leftPane.setBottom(tagEditorScrollPane);
 
-	// NOW FOR THE RIGHT
-	rightPane = new TabPane();
-	htmlView = new WebView();
-	htmlEngine = htmlView.getEngine();
-	cssEditor = new TextArea();
+        // NOW FOR THE RIGHT
+        rightPane = new TabPane();
+        htmlView = new WebView();
+        htmlEngine = htmlView.getEngine();
+        cssEditor = new TextArea();
 
-	// PUT BOTH ITEMS IN THE TAB PANE
-	Tab htmlTab = new Tab();
-	htmlTab.setText("HTML");
-	htmlTab.setContent(htmlView);
+        // PUT BOTH ITEMS IN THE TAB PANE
+        Tab htmlTab = new Tab();
+        htmlTab.setText("HTML");
+        htmlTab.setContent(htmlView);
 
-	// NOW FOR THE CSS
-	Tab cssTab = new Tab();
-	cssTab.setText("CSS");
-	cssTab.setContent(cssEditor);
-	rightPane.getTabs().add(htmlTab);
-	rightPane.getTabs().add(cssTab);
+        // NOW FOR THE CSS
+        Tab cssTab = new Tab();
+        cssTab.setText("CSS");
+        cssTab.setContent(cssEditor);
+        rightPane.getTabs().add(htmlTab);
+        rightPane.getTabs().add(cssTab);
 
-	// SETUP THE RESPONSE TO CSS EDITING
-	cssEditor.textProperty().addListener(e -> {
-	    pageEditController.handleCSSEditing(cssEditor.getText());
-	});
+        // SETUP THE RESPONSE TO CSS EDITING
+        cssEditor.textProperty().addListener(e -> {
+            pageEditController.handleCSSEditing(cssEditor.getText());
+        });
 
-	// AND NOW PUT IT IN THE WORKSPACE
-	workspaceSplitPane = new SplitPane();
-	workspaceSplitPane.getItems().add(leftPane);
-	workspaceSplitPane.getItems().add(rightPane);
+        // AND NOW PUT IT IN THE WORKSPACE
+        workspaceSplitPane = new SplitPane();
+        workspaceSplitPane.getItems().add(leftPane);
+        workspaceSplitPane.getItems().add(rightPane);
 
-	// AND FINALLY, LET'S MAKE THE SPLIT PANE THE WORKSPACE
-	workspace = new Pane();
-	workspace.getChildren().add(workspaceSplitPane);
+        // AND FINALLY, LET'S MAKE THE SPLIT PANE THE WORKSPACE
+        workspace = new Pane();
+        workspace.getChildren().add(workspaceSplitPane);
 
         // NOTE THAT WE HAVE NOT PUT THE WORKSPACE INTO THE WINDOW,
-	// THAT WILL BE DONE WHEN THE USER EITHER CREATES A NEW
-	// COURSE OR LOADS AN EXISTING ONE FOR EDITING
-	workspaceActivated = false;
+        // THAT WILL BE DONE WHEN THE USER EITHER CREATES A NEW
+        // COURSE OR LOADS AN EXISTING ONE FOR EDITING
+        workspaceActivated = false;
 
-	// MAKE SURE THE FILE MANAGER HAS THE ROOT AND THEN
-	// EXPORT THE SITE TO THE temp DIRECTORY. THEN, LOAD
-	// IT INTO THE WEB ENGINE
-	dataManager.setHTMLRoot(htmlRoot);
-	fileManager.exportData(dataManager, TEMP_PAGE);
-	loadTempPage();
+        // MAKE SURE THE FILE MANAGER HAS THE ROOT AND THEN
+        // EXPORT THE SITE TO THE temp DIRECTORY. THEN, LOAD
+        // IT INTO THE WEB ENGINE
+        dataManager.setHTMLRoot(htmlRoot);
+        fileManager.exportData(dataManager, TEMP_PAGE);
+        loadTempPage();
     }
 
     /**
@@ -268,9 +263,13 @@ public class Workspace extends AppWorkspaceComponent {
      * @return The html engine for the Web view component.
      */
     public WebEngine getHTMLEngine() {
-	return htmlEngine;
+        return htmlEngine;
     }
 
+    public void disableButton(Button button, boolean selectable){
+        button.setDisable(selectable);
+    }
+    
     /**
      * Accessor method for getting the html tree, which contains all the tags
      * for the page being edited.
@@ -278,7 +277,7 @@ public class Workspace extends AppWorkspaceComponent {
      * @return The html tree being edited.
      */
     public TreeView getHTMLTree() {
-	return htmlTree;
+        return htmlTree;
     }
 
     /**
@@ -288,7 +287,7 @@ public class Workspace extends AppWorkspaceComponent {
      * @return The root node of the html tree currently being edited.
      */
     public TreeItem getHTMLRoot() {
-	return htmlRoot;
+        return htmlRoot;
     }
 
     /**
@@ -297,8 +296,8 @@ public class Workspace extends AppWorkspaceComponent {
      * @param initRoot The node to use to set the root.
      */
     public void setHTMLRoot(TreeItem initRoot) {
-	htmlTree.setRoot(initRoot);
-	htmlRoot = initRoot;
+        htmlTree.setRoot(initRoot);
+        htmlRoot = initRoot;
     }
 
     /**
@@ -309,18 +308,16 @@ public class Workspace extends AppWorkspaceComponent {
      */
     @Override
     public void initStyle() {
-	// NOTE THAT EACH CLASS SHOULD CORRESPOND TO
-	// A STYLE CLASS SPECIFIED IN THIS APPLICATION'S
-	// CSS FILE
-	tagToolbar.getStyleClass().add(CLASS_BORDERED_PANE);
-	for (Button b : tagButtons) {
-	    b.getStyleClass().add(CLASS_TAG_BUTTON);
+        // NOTE THAT EACH CLASS SHOULD CORRESPOND TO
+        // A STYLE CLASS SPECIFIED IN THIS APPLICATION'S
+        // CSS FILE
+        tagToolbar.getStyleClass().add(CLASS_BORDERED_PANE);
+        for (Button b : tagButtons) {
+            b.getStyleClass().add(CLASS_TAG_BUTTON);
         }
-	leftPane.getStyleClass().add(CLASS_MAX_PANE);
-        tagToolbar.setStyle("-fx-border-color: black");
-	treeScrollPane.getStyleClass().add(CLASS_MAX_PANE);
-        treeScrollPane.setStyle("-fx-border-color: blue");
-	tagEditorLabel.getStyleClass().add(CLASS_HEADING_LABEL);
+        leftPane.getStyleClass().add(CLASS_MAX_PANE);
+        treeScrollPane.getStyleClass().add(CLASS_MAX_PANE);
+        tagEditorLabel.getStyleClass().add(CLASS_HEADING_LABEL);
     }
 
     /**
@@ -329,74 +326,79 @@ public class Workspace extends AppWorkspaceComponent {
      */
     @Override
     public void reloadWorkspace() {
-	try {
-	    // WE DON'T WANT TO RESPOND TO EVENTS FORCED BY
-	    // OUR INITIALIZATION SELECTIONS
-	    pageEditController.enable(false);
+        try {
+            // WE DON'T WANT TO RESPOND TO EVENTS FORCED BY
+            // OUR INITIALIZATION SELECTIONS
+            pageEditController.enable(false);
 
-	    // FIRST CLEAR OUT THE OLD STUFF
-	    tagPropertyLabels.clear();
-	    tagPropertyTextFields.clear();
-	    tagEditorPane.getChildren().clear();
+            // FIRST CLEAR OUT THE OLD STUFF
+            tagPropertyLabels.clear();
+            tagPropertyTextFields.clear();
+            tagEditorPane.getChildren().clear();
 
-	    // FIRST ADD THE LABEL
-	    tagEditorPane.add(tagEditorLabel, 0, 0, 2, 1);
+            // FIRST ADD THE LABEL
+            tagEditorPane.add(tagEditorLabel, 0, 0, 2, 1);
 
-	    // THEN LOAD IN ALL THE NEW STUFF
-	    TreeItem selectedItem = (TreeItem) htmlTree.getSelectionModel().getSelectedItem();
-	    if (selectedItem != null) {
-		HTMLTagPrototype selectedTag = (HTMLTagPrototype) selectedItem.getValue();
-		HashMap<String, String> attributes = selectedTag.getAttributes();
-		Collection<String> keys = attributes.keySet();
-		int row = 1;
-		for (String attributeName : keys) {
-		    String attributeValue = selectedTag.getAttribute(attributeName);
-		    Label attributeLabel = new Label(attributeName + ": ");
-		    attributeLabel.getStyleClass().add(CLASS_PROMPT_LABEL);
-		    TextField attributeTextField = new TextField(attributeValue);
-		    attributeTextField.getStyleClass().add(CLASS_PROMPT_TEXT_FIELD);
-		    tagEditorPane.add(attributeLabel, 0, row);
-		    tagEditorPane.add(attributeTextField, 1, row);
-		    attributeTextField.textProperty().addListener(e -> {
-			// UPDATE THE TEMP SITE AS WE TYPE ATTRIBUTE VALUES
-			pageEditController.handleAttributeUpdate(selectedTag, attributeName, attributeTextField.getText());
-		    });
-		    row++;
-		}
-	    }
+            // THEN LOAD IN ALL THE NEW STUFF
+            TreeItem selectedItem = (TreeItem) htmlTree.getSelectionModel().getSelectedItem();
+            if (selectedItem != null) {
+                HTMLTagPrototype selectedTag = (HTMLTagPrototype) selectedItem.getValue();
+                String tagName = selectedTag.getTagName();
+               // if (!isLegalParent(tagName)) {
+                                    
+                //}
+                HashMap<String, String> attributes = selectedTag.getAttributes();
+                Collection<String> keys = attributes.keySet();
+                int row = 1;
+                for (String attributeName : keys) {
+                    String attributeValue = selectedTag.getAttribute(attributeName);
+                    Label attributeLabel = new Label(attributeName + ": ");
+                    attributeLabel.getStyleClass().add(CLASS_PROMPT_LABEL);
+                    TextField attributeTextField = new TextField(attributeValue);
+                    attributeTextField.getStyleClass().add(CLASS_PROMPT_TEXT_FIELD);
+                    tagEditorPane.add(attributeLabel, 0, row);
+                    tagEditorPane.add(attributeTextField, 1, row);
+                    attributeTextField.textProperty().addListener(e -> {
+                        // UPDATE THE TEMP SITE AS WE TYPE ATTRIBUTE VALUES
+                        pageEditController.handleAttributeUpdate(selectedTag, attributeName, attributeTextField.getText());
+                    });
+                    row++;
+                }
+            }
+            
 
-	    // LOAD THE CSS
-	    DataManager dataManager = (DataManager) app.getDataComponent();
-	    cssEditor.setText(dataManager.getCSSText());
+            // LOAD THE CSS
+            DataManager dataManager = (DataManager) app.getDataComponent();
+            cssEditor.setText(dataManager.getCSSText());
 
-	    // THEN FORCE THE CHANGES TO THE TEMP HTML PAGE
-	    FileManager fileManager = (FileManager) app.getFileComponent();
-	    fileManager.exportData(dataManager, TEMP_PAGE);
+            // THEN FORCE THE CHANGES TO THE TEMP HTML PAGE
+            FileManager fileManager = (FileManager) app.getFileComponent();
+            fileManager.exportData(dataManager, TEMP_PAGE);
 
-	    // WE DON'T WANT TO RESPOND TO EVENTS FORCED BY
-	    // OUR INITIALIZATION SELECTIONS
-	    pageEditController.enable(true);
-	} catch (Exception e) {
-	    AppMessageDialogSingleton dialog = AppMessageDialogSingleton.getSingleton();
-	    PropertiesManager props = PropertiesManager.getPropertiesManager();
-	    dialog.show(props.getProperty(UPDATE_ERROR_TITLE), props.getProperty(UPDATE_ERROR_MESSAGE));
-	}
+            // WE DON'T WANT TO RESPOND TO EVENTS FORCED BY
+            // OUR INITIALIZATION SELECTIONS
+            pageEditController.enable(true);
+        } catch (Exception e) {
+            AppMessageDialogSingleton dialog = AppMessageDialogSingleton.getSingleton();
+            PropertiesManager props = PropertiesManager.getPropertiesManager();
+            dialog.show(props.getProperty(UPDATE_ERROR_TITLE), props.getProperty(UPDATE_ERROR_MESSAGE));
+        }
     }
 
     /**
      * This function loads the temp page into the web view.
      */
     public void loadTempPage() {
-	String urlPath = TEMP_PAGE;
-	File webPageFile = new File(urlPath);
-	try {
-	    URL pageURL = webPageFile.toURI().toURL();
-	    String pagePath = pageURL.toString();
-	    htmlEngine.load(pagePath);
-	} catch (MalformedURLException murle) {
-	    PropertiesManager props = PropertiesManager.getPropertiesManager();
-	    AppMessageDialogSingleton dialog = AppMessageDialogSingleton.getSingleton();
-	    dialog.show(props.getProperty(TEMP_PAGE_LOAD_ERROR_TITLE), props.getProperty(TEMP_PAGE_LOAD_ERROR_MESSAGE));
-	}
+        String urlPath = TEMP_PAGE;
+        File webPageFile = new File(urlPath);
+        try {
+            URL pageURL = webPageFile.toURI().toURL();
+            String pagePath = pageURL.toString();
+            htmlEngine.load(pagePath);
+        } catch (MalformedURLException murle) {
+            PropertiesManager props = PropertiesManager.getPropertiesManager();
+            AppMessageDialogSingleton dialog = AppMessageDialogSingleton.getSingleton();
+            dialog.show(props.getProperty(TEMP_PAGE_LOAD_ERROR_TITLE), props.getProperty(TEMP_PAGE_LOAD_ERROR_MESSAGE));
+        }
     }
 }
